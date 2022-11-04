f9a1858a6e0e6f1faf828292115c951cdd4d28bc
david-jughead <makutososa300@gmail.com>
2022-11-04 18:04:10 -0400

    contador y buscador

--- src/App.js
+++ src/App.js
@@ -6,22 +6,50 @@ import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de intro a react', completed: false },
   { text: 'LLorar con la llorona', completed: false },
 ]
 
 function App() {
+  const [ todos, setTodos ] = React.useState(defaultTodos);
+  const [ searchValue, setSearchValue ] = React.useState('');
+  
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();  
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);  
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
 
-      <TodoSearch />
+      <TodoSearch
+        searchValue={searchValue} 
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
-        {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+        {searchedTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed} 
+          />
         ))}
       </TodoList>
 
