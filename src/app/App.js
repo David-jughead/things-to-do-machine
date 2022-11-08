f88735fb9c1fbeafd3efbc505da23e63a25f62ef
david-jughead <makutososa300@gmail.com>
2022-11-08 14:41:23 -0400

    manejo de efectos

--- src/app/App.js
+++ src/app/App.js
@@ -9,32 +9,56 @@ import { AppUI } from './AppUI'
 // ]
 
 function useLocalStorange(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+  const [error, setError] = React.useState(false);
+  const [loading, setLoanding] = React.useState(true);
+  const [ item, setItem ] = React.useState(initialValue);
 
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
 
-  const [ item, setItem ] = React.useState(parsedItem);
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+        setLoanding(false);
+      } catch (error) {
+        setError(error);
+      }
+    }, 1000); 
+  });
 
   const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      setError(error);
+    }
   };
 
-  return [
+  return {
     item,
     saveItem,
-  ];
+    loading,
+    error,
+  };
 }
 
 function App() {
-  const [todos, saveTodos] = useLocalStorange('Todos_v1', []);
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorange('Todos_v1', []);
   const [ searchValue, setSearchValue ] = React.useState('');
   
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -67,7 +91,9 @@ function App() {
   };
 
   return (
-    <AppUI 
+    <AppUI
+      loading={loading} 
+      error={error} 
       totalTodos={totalTodos}
       completedTodos={completedTodos}
       searchValue={searchValue} 
