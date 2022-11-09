aec33b4e8b210d857ba43e8d6685360fe5b06397
david-jughead <makutososa300@gmail.com>
2022-11-08 22:52:11 -0400

    modal

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import './CreateTodoButton.css'
 
 function CreateTodoButton() {
-  const onClickButton = (msg) => {
-    alert(msg);
+  const { openModal, setOpenModal } = React.useContext(TodoContext)
+  const onClickButton = () => {
+    setOpenModal(!openModal);
   }
   
   return (
     <button 
       className="CreateTodoButton"
-      onClick={() => onClickButton('Aqui se deberia abrir el modal')}  
+      onClick={() => onClickButton()}  
     >
       +
     </button>
